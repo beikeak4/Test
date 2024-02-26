1a7c56b41999f8f92e2cb824a77ab804d67f7f4b
beikeak4 <2056626751@qq.com>
2024-02-26 20:31:51 +0800

    ceshitijiao2

--- src/main/java/com/example/demo/comment.java
+++ src/main/java/com/example/demo/comment.java
@@ -3,6 +3,7 @@ package com.example.demo;
 
 public class comment {
     public static void main(String[] args) {
+        //
         System.out.println("This is a comment");
         System.out.println("This is a comment");
     }
